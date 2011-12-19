3f3f888a71033755a6e49149abd73f4e0b114a8e
Peter Krnjevic <pkrnjevic@gmail.com>
2011-12-18 23:50:38 -0500

    removed ENV.gcc_4_2: xcode 4.2 has llvm-gcc-4.2, missing gcc-4.2 on Lion

--- Library/Formula/gmp.rb
+++ Library/Formula/gmp.rb
@@ -16,7 +16,7 @@ class Gmp < Formula
     # Reports of problems using gcc 4.0 on Leopard
     # https://github.com/mxcl/homebrew/issues/issue/2302
     # Also force use of 4.2 on 10.6 in case a user has changed the default
-    ENV.gcc_4_2
+#    ENV.gcc_4_2
 
     args = ["--prefix=#{prefix}", "--enable-cxx"]
 
